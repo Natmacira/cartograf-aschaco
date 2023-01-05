c9d9f6429807b0ac6da813855dc04657ff7d7e58
natmacira <natimciraolo@gmail.com>
2023-01-05 15:05:48 -0300

    proof of concept

---
+++ api_v3.php
@@ -0,0 +1,48 @@
+
+{
+  "status": "OK",
+  "result": {
+    "batchcomplete": "",
+    "query": {
+      "userinfo": {
+        "id": 11835224,
+        "name": "Natmacira",
+        "groups": [
+          "*",
+          "user",
+          "autoconfirmed"
+        ],
+        "rights": [
+          "read",
+          "edit",
+          "createpage",
+          "createtalk",
+          "writeapi",
+          "translate",
+          "abusefilter-view",
+          "abusefilter-log",
+          "flow-hide",
+          "item-term",
+          "property-term",
+          "item-merge",
+          "item-redirect",
+          "property-create",
+          "mediainfo-term",
+          "upload",
+          "reupload-own",
+          "move-categorypages",
+          "minoredit",
+          "purge",
+          "applychangetags",
+          "imagelabel-review",
+          "move",
+          "autoconfirmed",
+          "editsemiprotected",
+          "skipcaptcha",
+          "abusefilter-log-detail",
+          "flow-edit-post"
+        ]
+      }
+    }
+  }
+}
