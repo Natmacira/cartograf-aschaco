ad6d6663d81cd67d6a4251e6fbce0c877deed74c
natmacira <natimciraolo@gmail.com>
2023-02-04 22:26:52 -0300

    home html sections until logos included

---
+++ home.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once 'header.php';
+?>
+
+<section class="map-section">
+    <p class="highlighted-text">En esta plataforma encontrarás un mapa que permite visualizar, cargar y editar contenido relativo a la historia y cultura de los pueblos indígenas del Chaco:
+        Qom, Wichi y Moqoit.</p>
+    <embed src="" type="">
+    <a href="form.php">Subí tu foto / audio / video</a>
+    <p class="highlighted-text">Mirá el video para aprender cómo subir contenido</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/9e_d4DNJmZk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <p>El contenido puede tratarse de fotos, videos, audio, documentos escritos (antiguos y actuales). Tené en cuenta que puede ser contenido sensible para los comunidades indígenas.</p>
+</section>
+<section class="divider">
+    <img src="img/mobile/separador-colores.png" alt="Separador de secciones en la pantalla">
+</section>
+
+<section class="who-we-are">
+    <article class="what-is">
+        <h1>¿Qué es Cartografías Abiertas: Qom, Wichí, Moqoit?</h1>
+        <p>Esta plataforma colaborativa, multimedia y plurilingüe es
+            resultado de un proyecto conjunto entre el Centro de
+            Documentación Indígena No’lhametwet del Chaco y la Asociación
+            Civil Wikimedia Argentina. Su objetivo es poner en circulación
+            los conocimientos y la cultura de las identidades indígenas de
+            la provincia chaqueña desde sus propias voces y visiones.
+            La iniciativa fue ejecutada tras salir ganador de la
+            convocatoria “Cultura, Ciencia y Territorio 2022” impulsada
+            por Fundación Williams y Potenciar.
+        </p>
+        <figure>
+            <img src="img/mobile/Artesana_Cestera.png" alt="">
+            <figcaption>Artesana Cestera (Esteban Zugasti, 1960). Colección de fotografía de Esteban Zugasti. Centro de documentación indígena No'lhametwet.</figcaption>
+        </figure>
+    </article>
+    <article class="how-work">
+        <h2>¿En qué consiste la plataforma?</h2>
+        <p>A través de la plataforma, el patrimonio indígena custodiado por diferentes instituciones del Chaco integra un mapa interactivo vinculado a los Proyectos Wikimedia. Esto permite que cualquier persona pueda acceder, editar y usar el contenido libremente, según las pautas de las licencias que lo rigen.
+            <br>
+            De la misma forma, personas indígenas pueden visualizar, cargar y modificar el contenido volcado en el mapa para que sea verdaderamente representativo de su cultura.
+            <br>
+            La interfaz de la plataforma está traducida al qom, wichí, moqoit y español.
+        </p>
+    </article>
+    <article class="who-participates">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/9e_d4DNJmZk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </article>
+    <article class="who-colaborates">
+        <h2>¿Quiénes pueden cargar contenido en la plataforma?</h2>
+        Pueden cargar contenido personas vinculadas a los tres pueblos/naciones indígenas que habitan el territorio chaqueño: qom, wichí y moqoit.
+        <br>
+        También pueden cargar personas no indígenas, que deseen compartir respetuosamente material vinculado a la cultura de alguno de estos tres pueblos. En cualquier caso, necesitarán un usuario y contraseña que los habilitará a modificar y cargar contenido, e indicar con qué pueblo/nación se identifican.
+        <figure>
+            <img src="img/mobile/Mujer_trabajando.png" alt="Mujer trabajando en el campo de Pampa Aguará en el año 1955.">
+            <figcaption>
+                Mujer trabajando en el campo de Pampa Aguará en el año 1955.
+            </figcaption>
+        </figure>
+    </article>
+    <article class="collaborators">
+        <h2>collaborators</h2>
+        <p>Este proyecto es posible gracias a la colaboración de las siguientes instituciones culturales y educativas:</p>
+        <div class="sponsors-logos">
+            <img src="img/mobile/logo-wiki.png" alt="Logo wikimedia">
+            <img src="img/mobile/logo-centrodoc.png" alt="Logo Centro de documentación indigena">
+            <img src="img/mobile/logo-cultura.png" alt="Logo Programa de cultura de los pueblos originarios">
+            <img src="img/mobile/logo-potenciar.png" alt="Logo Potenciar">
+            <img src="img/mobile/logo-lab.png" alt="Logo Laboratorio de innovación cultural y cientifica">
+            <img src="img/mobile/logo-fundacionw.png" alt="Logo Fundación Williams">
+        </div>
+        <div class="collaborators">
+            <img src="img/mobile/logo-subsecretariapluri.png" alt="Logo subsecretaria plurilingÜismo">
+            <img src="img/mobile/logo-fadycc-UNNE.png" alt="Logo Facultad de Artes, Diseño y Ciencias de la cultura">
+            <img src="img/mobile/Logo-ICCH_.png" alt="Logo Instituto de cultura.">
+        </div>
+        <div class="collaborators">
+            <img src="img/mobile/logo-museo-hombre-chaq.png" alt="Logo museo del hombre chaqueño">
+            <img src="img/mobile/logo-museo-historico-reg.png" alt="Logo Museo histórico regional.">
+            <img src="img/mobile/logo-museo-medios.png" alt="Logo Museo de medios de comunicación.">
+            <img src="img/mobile/logo-archivo-historico.png" alt="Logo Archivo histórico.">
+            <img src="img/mobile/logo-conicet.png" alt="Logo Conicet">
+            <img src="img/mobile/logo-DCI-UNNE.png" alt="Logo lincenciatura de ciencias de la información.">
+            <img src="img/mobile/logo-DPC.png" alt="Logo Dirección de patrimonio cultural.">
+        </div>
+    </article>
+</section>
+
+<?php
+
+require_once 'footer.php';
