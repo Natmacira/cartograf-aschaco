a7f73aa20e5f01ff94554c6a14f9143cfa1751f7
natmacira <natimciraolo@gmail.com>
2023-02-08 11:50:10 -0300

    styles for upload form

--- form.php
+++ form.php
@@ -20,7 +20,7 @@ require_once 'header.php';
 <form action="validation-form.php">
     <p><strong>Completá el siguiente formulario con tus datos</strong></p>
 
-    <label for="name">Nombre <sup>*</sup><input type="text" id="name" name="nombre"  value="name" required> </label><br>
+    <label for="name">Nombre <sup>*</sup><input type="text" id="name" name="nombre"  value="" required placeholder="escribí tu nombre"> </label><br>
     <label for="nation">Pueblo/Nación <sup>*</sup></label> <br>
     <label for="qom" class="nation-option-label"><input type="radio" id="qom" name="nation" value="1" required>Qom </label><br>
     <label for="wichi" class="nation-option-label"><input type="radio" id="wichi" name="nation" value="2">Wichi </label><br>
@@ -28,10 +28,10 @@ require_once 'header.php';
     <label for="otro" class="nation-option-label"><input type="radio" id="otro" name="nation" value="4">
     Otro:<input type="text" name="nation" class="other-nation-input" placeholder="ej. Argentina">
     </label><br>
-    <label for="parcialidad">Parcialidad<input type="text" id="parcialidad" name="parcialidad" value="parcialidad"> </label><br>
-    <label for="comunidad">Comunidad<input type="text" id="comunidad" name="comunidad" value="comunidad"> </label><br>
-    <label for="institucion">Nombre de la institución<input type="text" id="institucion" name="institucion" value="institucion"> </label><br>
-    <label for="terminos"><input type="checkbox" id="terminos" name="terminos" value="terminos">Acepto los <a href="">términos y condiciones</a></label><br>
+    <label for="parcialidad">Parcialidad (opcional)<input type="text" id="parcialidad" name="parcialidad" value="" placeholder="indicá tu parcialidad"> </label><br>
+    <label for="comunidad">Comunidad (opcional)<input type="text" id="comunidad" name="comunidad" value="" placeholder="indicá tu comunidad"> </label><br>
+    <label for="institucion">Si pertenecés a una institución indicá su nombre (opcional)<input type="text" id="institucion" name="institucion" value="" placeholder="nombre de la institución"> </label><br>
+    <label for="terminos" id="terms-label" ><input type="checkbox" id="terminos" name="terminos" value="" required>Acepto los <a href="">términos y condiciones</a></label><br>
     <button>Siguiente</button>
 
 </form>
