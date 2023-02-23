f77688115983add9a4da51c7747c02fb14f80861
natmacira <natimciraolo@gmail.com>
2023-02-23 16:40:43 -0300

    added terms qom

--- terminos-Qom.php
+++ terminos-Qom.php
@@ -1,20 +1,25 @@
 <?php
 $body_class = "terms";
-require_once 'header.php';
+require_once 'header-Qom.php';
 ?>
 
 <section class="terms-and-conditions">
 <h1>Términos y condiciones</h1>
-<p>Esta plataforma utiliza proyectos wikimedia (Wikimedia Commons, Wikidata, Wikipedia) como soporte para almacenar y vincular contenidos digitales relacionados a los pueblos indígenas. Esto implica que los usuarios y usuarias de este sitio aceptan publicar contenido bajo los términos de las licencias permitidas en el marco de los proyectos wikimedia.</p>
+<p>ʼ ʼo W K D ( W K D S W K D W K PWD ) ʼ  ỹ maxat- ʼ ʼo ỹasaxasheguemec Anam lainternet digital maye  iỹ o . ʼ ʼo o ʼo  
+ o o W K D . </p>
 <img src="img/mobile/CC_BY.png" alt="Logo Creative Commons BY 4.0 Internacional">
-<p>CreativeCommons BY 4.0 Internacional: La licencia permite a las personas compartir, copiar y redistribuir libremente el material en cualquier medio o formato y adaptar, remezclar, transformar y construir a partir del material con cualquier propósito, incluso comercial. Las condiciones son dar crédito de manera adecuada, brindar un enlace a la licencia, e indicar si se han realizado cambios.</p>
+<p>Am qoỹami ra ishet ra ancompartir, ancopian, ʼ ʼ   ʼo o ʼo ʼo ʼo  
+ o ʼ o ʼo . 
+</p>
 <img src="img/mobile/CC_BY-SA.png" alt="Logo Creative Commons BY SA 4.0 Internacional">
-<p>CreativeCommons BY SA 4.0 Internacional: La presente licencia implica los mismos derechos y obligaciones que la CC BY 4.0 internacional previamente descrita. Adicionalmente, si se remezcla, transforma o crea a partir del material, se debe distribuir su contribución bajo la misma licencia del original.</p>
-<p>En caso de no ser los autores o autoras del contenido, éstos tendrán que estar en dominio público (no poseer derechos de autor vigentes) o bien ser publicados con las licencias mencionadas por quienes dispongan de la explotación de dichos derechos. Si tenés alguna duda con esto, escribinos a cultura@wikimedia.org.ar.</p>
+<p>Anam am qoỹami anlicenci ihuen ʼ ỹ o p ʼ  
+ BY .0 p ʼ o ʼ o o ʼo   ʼ qaỹasateguet nache ishet ra qailagaxatec qo qaỹasaxasheguem. 
+</p>
+<p>Cam ahuahuotaique ahuasaxasheguem ihuen ra ỹo ʼ ‘  ʼo o ʼo -lla mashe nanet-talec nache am qan ñe ʼ op. R o ʼo   ʼ ỹ ʼ @ .o . .</p>
 
 </section>
 
 
 <?php
-require_once 'footer.php';
+require_once 'footer-Qom.php';
 ?>
