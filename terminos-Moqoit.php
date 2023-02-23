5ceed2e8041232b78635dfa5424a096817448975
natmacira <natimciraolo@gmail.com>
2023-02-23 16:36:25 -0300

    added terms moqoit

---
+++ terminos-Moqoit.php
@@ -0,0 +1,24 @@
+<?php
+$body_class = "terms";
+require_once 'header-Moqoit.php';
+?>
+
+<section class="terms-and-conditions">
+<h1>Términos chaqai nelaataxaset</h1>
+<p>Nataxare> plataforma ỹouen proyecto Wikimedia (Wikimedia Commons,Wikidata,  Wikipedia) chaqai>neeta ledaxaset quiyim ỹa>maxat chaqai necoñiguito> na>maq  nedaxaseteripi hueetaguilo quiyim ichoxotetac na>maq Moqoit chaqai naua liỹaripi  no>ueenataxac. Najho ỹalecqajno quena> usuario chaqai usuaria nataxare>  ỹacoñiguit >ishit inaxashiguim na>maq lo>ueenatec hueeta>ñot na> términos de la  licencia ỹasen quena>maq hueetougui só>maq proyecto wikimedia.</p>
+<img src="img/mobile/CC_BY.png" alt="Logo Creative Commons BY 4.0 Internacional">
+<p>CreativeCommons BY 4.0 Internacional: Na>ta licencia ỹasenta> quiyim na>maq  qom >ishit >iuagaxatec, ỹadenaxat chaqai imeenec naua lo>ueenatec canchaqaica  medio loqo>m formato chaqai ỹacona, ỹaleqanta>, nadalaxashiguim chaqai ỹa>maxajñi na>maq lo>ueenatec canchaqaica taỹaugui, qa>nem >ishit ichoxot da  >ue ca> lemeenec. Na>maq nelaataxaset ỹaanem óna enlace quena>maq licencia, 
+     qaiyi ishit ichoxot da> >ue qa> ladalqatec  </p>
+<img src="img/mobile/CC_BY-SA.png" alt="Logo Creative Commons BY SA 4.0 Internacional">
+<p>CreativeCommons BY SA 4.0 Internacional: Nataxare> licencia ỹalecqajno chaqa>nem na> llicraxac chaqai nelaataxaset quiyi CC BY 4.0 internacional  queso>maq taỹaugui. 
+Da> ỹalecqajno, ỹalecqajñiguit, nadalaxashiguim loqo>m ỹo>uet na>maq  lo>ueenatec, qaiyi >ishit imeenec qalaq se>ishit napacalec na>maq na> licencia  auaxaic. 
+</p>
+<p>Da> mesqa lo>ueenatec najho no>ueenatec hueetagui nata> plataforma, quiyim  da> ỹouen na>maq no>ueenatec hueetaugui na> mashic neuanatec (mesqa  no>coota ná>maq llicraxac só>maq auaxaic lo>ueenatec) 
+Loqo>m da> mashic qa>inaxashiguim qa>ỹouen qaỹahotai na> licencia qa>maq  >auaxaic hueetagui da> qaỹasenta so>maq >auaxaic lo>ueenatec. No>m >ue  repeetaxanaxahi quena>maq najho, >vité qayirquí> quena> a cultura@wikimedia.org.ar.</p>
+
+</section>
+
+
+<?php
+require_once 'footer-Moqoit.php';
+?>
---
+++ terminos-Qom.php
@@ -0,0 +1,20 @@
+<?php
+$body_class = "terms";
+require_once 'header.php';
+?>
+
+<section class="terms-and-conditions">
+<h1>Términos y condiciones</h1>
+<p>Esta plataforma utiliza proyectos wikimedia (Wikimedia Commons, Wikidata, Wikipedia) como soporte para almacenar y vincular contenidos digitales relacionados a los pueblos indígenas. Esto implica que los usuarios y usuarias de este sitio aceptan publicar contenido bajo los términos de las licencias permitidas en el marco de los proyectos wikimedia.</p>
+<img src="img/mobile/CC_BY.png" alt="Logo Creative Commons BY 4.0 Internacional">
+<p>CreativeCommons BY 4.0 Internacional: La licencia permite a las personas compartir, copiar y redistribuir libremente el material en cualquier medio o formato y adaptar, remezclar, transformar y construir a partir del material con cualquier propósito, incluso comercial. Las condiciones son dar crédito de manera adecuada, brindar un enlace a la licencia, e indicar si se han realizado cambios.</p>
+<img src="img/mobile/CC_BY-SA.png" alt="Logo Creative Commons BY SA 4.0 Internacional">
+<p>CreativeCommons BY SA 4.0 Internacional: La presente licencia implica los mismos derechos y obligaciones que la CC BY 4.0 internacional previamente descrita. Adicionalmente, si se remezcla, transforma o crea a partir del material, se debe distribuir su contribución bajo la misma licencia del original.</p>
+<p>En caso de no ser los autores o autoras del contenido, éstos tendrán que estar en dominio público (no poseer derechos de autor vigentes) o bien ser publicados con las licencias mencionadas por quienes dispongan de la explotación de dichos derechos. Si tenés alguna duda con esto, escribinos a cultura@wikimedia.org.ar.</p>
+
+</section>
+
+
+<?php
+require_once 'footer.php';
+?>
