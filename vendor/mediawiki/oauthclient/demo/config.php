37c54408ca2c9b8f808eb63cb27b740a6c76a744
Lucía Cáceres <jlmcaceres086@gmail.com>
2023-03-23 17:42:13 -0300

    Added testing files.

---
+++ vendor/mediawiki/oauthclient/demo/config.php
@@ -0,0 +1,27 @@
+<?php
+
+// To get this demo working, you need to go to this wiki and register a new OAuth consumer.
+// Not that this URL must be of the long form with 'title=Special:OAuth', and not a clean URL.
+$oauthUrl = 'https://meta.wikimedia.beta.wmflabs.org/w/index.php?title=Special:OAuth';
+
+// When you register, you will get a consumer key and secret. Put these here (and for real
+// applications, keep the secret secret! The key is public knowledge.).
+$consumerKey    = '4dcc9c276f0c8b2e59d405e8e67f9c77';
+$consumerSecret = '35e5203f94ec1c71dbacc8cc62812fe219cf7505';
+
+$consumerKey    = '1bf802ebc6a7dcbfc44f346f571f8998';
+$consumerSecret = 'd17df20b22052b21da58f8f0b7ee017fb2bba82b';
+
+$consumerKey    = '4acd76aae1bf0aa0cab4997aec1447a1';
+$consumerSecret = 'fca7b2f7d83fbb0aa6c9fbadd905eb12cebeb3e8';
+
+/**
+ * Consumer token
+4dcc9c276f0c8b2e59d405e8e67f9c77
+Consumer secret
+35e5203f94ec1c71dbacc8cc62812fe219cf7505
+Access token
+4acd76aae1bf0aa0cab4997aec1447a1
+Access secret
+fca7b2f7d83fbb0aa6c9fbadd905eb12cebeb3e8
+ */
--- vendor/mediawiki/oauthclient/demo/index.php
+++ vendor/mediawiki/oauthclient/demo/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Require the library and set up the classes we're going to use in this first part.
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use MediaWiki\OAuthClient\Client;
 use MediaWiki\OAuthClient\ClientConfig;
